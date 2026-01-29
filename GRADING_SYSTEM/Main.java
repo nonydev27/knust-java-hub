fa2224c497d6415e22a8bf34c610b7d05b8198af
nonydev <karldjansi123@gmail.com>
2026-01-29 15:32:32 +0000

    ...

--- GRADING_SYSTEM/Main.java
+++ GRADING_SYSTEM/Main.java
@@ -10,29 +10,26 @@ public class Main {
         System.out.println(" Enter Exam Score: ");
         examScore = input.nextInt();
 
-        
         System.out.println(" Enter Assesment Score: ");
         assesment = input.nextInt();
 
         System.out.println(" Enter Amount Paid(Fees): ");
         Fees = input.nextInt();
 
-      
         boolean isPassed = false;
         boolean isCondoned = false;
 
-        // So this checks whether the exam has been passed or not
+    
         if (examScore >= 25 && assesment >= 15) {
             isPassed = true;
         } 
-        // 2. Check Condoned (Only if standard pass failed)
-        // Logic: Total must be 39, split as 24/15 or 25/14
+      
         else if ((examScore == 24 && assesment == 15) || (examScore == 25 && assesment == 14)) {
             isPassed = true;
             isCondoned = true;
         }
 
-        // 3. Final Output Logic
+     
         if (isPassed) {
             if (isCondoned) {
                 System.out.println("Academic Status: PASSED (CONDONED)");
@@ -40,15 +37,25 @@ public class Main {
                 System.out.println("Academic Status: PASSED");
             }
 
-            // Check Fees (Only matters if they passed)
+         
             if (Fees >= 100) {
                 System.out.println("Result: CERTIFICATE ISSUED");
             } else {
                 System.out.println("Result: NO CERTIFICATE (Fees unpaid)");
             }
+
+          
+
+                 if ((examScore != 24  && assesment != 15) || (examScore !=25 && assesment != 14)){
+                System.out.println("You have failed to meet the necesary pass mark!");
+            }
+       
+
         } else {
             System.out.println("Academic Status: FAILED");
             System.out.println("Result: REPEATED");
         }
+
+        
     }
 }
