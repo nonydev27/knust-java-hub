3da68417d0c81fd1354f773580f4189ff008f437
nonydev <karldjansi123@gmail.com>
2026-02-04 14:08:22 +0000

    ...

---
+++ Greetings/Pain.java
@@ -0,0 +1,19 @@
+package Greetings;
+import java.util.Scanner;
+
+public class Pain {
+    public static void main(String[] args) {
+        System.out.println("This program takes the name of the user and then displays a greeting!");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isBlank()) { 
+            System.err.println("Enter your name: ");
+            scanner.nextLine();
+        }
+
+        System.out.println("Good day "+name);
+        scanner.close();
+    }}
+}
