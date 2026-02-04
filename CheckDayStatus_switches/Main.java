f7dcce0edda5ffddb9edfa282218cbb68065c113
nonydev <karldjansi123@gmail.com>
2026-02-04 13:34:38 +0000

    ...

--- CheckDayStatus_switches/Main.java
+++ CheckDayStatus_switches/Main.java
@@ -3,8 +3,8 @@ package CheckDayStatus_switches;
     public static void main(String[] args) {
         
         System.out.println("THIS PROGRAM IS DESIGNED TO ALTERNATE USING SWITCHES");
-SSS
-        String day = "Monday";
+
+        String day = "Saturday";
 
         switch(day){
             case "Monday" : System.out.println("It is Monday!");
@@ -14,9 +14,13 @@ SSS
             case "Wednesday" : System.out.println("It is Wednesday!");
             break;
             case "Thursday" : System.out.println("It is Thursday!");
+            break;
             case "Friday" : System.out.println("It is Friday");
+            break;
             case "Saturday" : System.out.println("It is Saturday!");
+            break;
             case "Sunday" : System.out.println("It is Sunday!");
+            break;
             default : System.out.println("Day does not exist!");
         };
 
