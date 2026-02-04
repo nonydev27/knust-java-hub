40b7aac57dd37ed3276bc8eb16268468cd463869
nonydev <karldjansi123@gmail.com>
2026-02-04 13:35:07 +0000

    ...

--- CheckDayStatus_switches/Main.java
+++ CheckDayStatus_switches/Main.java
@@ -9,18 +9,25 @@ package CheckDayStatus_switches;
         switch(day){
             case "Monday" : System.out.println("It is Monday!");
             break;
+
             case "Tuesday" : System.out.println("It is Tuesday!");
             break;
+
             case "Wednesday" : System.out.println("It is Wednesday!");
             break;
+
             case "Thursday" : System.out.println("It is Thursday!");
             break;
+
             case "Friday" : System.out.println("It is Friday");
             break;
+
             case "Saturday" : System.out.println("It is Saturday!");
             break;
+
             case "Sunday" : System.out.println("It is Sunday!");
             break;
+            
             default : System.out.println("Day does not exist!");
         };
 
