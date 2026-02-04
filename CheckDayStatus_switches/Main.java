64b24eebdb16a5bddfa974bafa6852146937f2e3
nonydev <karldjansi123@gmail.com>
2026-02-04 09:54:23 +0000

    ..

---
+++ CheckDayStatus_switches/Main.java
@@ -0,0 +1,7 @@
+package CheckDayStatus_switches;
+  public class Main{
+    public static void main(String[] args) {
+        
+        System.out.println("THIS PROGRAM IS DESIGNED TO ALTERNATE USING SWITCHES");
+    }
+};
