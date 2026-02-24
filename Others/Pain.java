90be7dfba4ba20caf3481b5bdbc277fa599931f1
nonydev <karldjansi123@gmail.com>
2026-02-24 23:28:38 +0000

    ...

---
+++ Others/Pain.java
@@ -0,0 +1,12 @@
+public class Pain {
+
+    /*Task: Write a method that reverses an array without creating a new/second array.
+
+    Input: [1, 2, 3, 4, 5]
+
+    Expected Output: [5, 4, 3, 2, 1] */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        
+    }}
