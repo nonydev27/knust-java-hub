d8666d9908c229f18a7dfb52b22cda9bc290facc
nonydev <karldjansi123@gmail.com>
2026-03-03 22:41:49 +0000

    ...

--- MobileDataMonitor/Main.java
+++ MobileDataMonitor/Main.java
@@ -28,7 +28,9 @@ Implement the following notification system based on the remaining balance:
 * Once the loop terminates, display the total data used and the final balance (formatted to 2 decimal places). */
 
 public static void main(String[] args){
-    System.out.println();
+    
+    double init_balance = 20.00;
+    
 }
 
 }
