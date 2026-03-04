5ce6cf8132585726e90fad4f79f732ceafd4bc85
nonydev <karldjansi123@gmail.com>
2026-03-04 09:55:16 +0000

    ...

--- GradingReport/Pain.java
+++ GradingReport/Pain.java
@@ -6,4 +6,12 @@ public class Pain {
     1. Store the scores of six students in an integer array
     2. Use a while loop to itererate through the array */
     
+    public static void main(String[] args){
+        char myChar = 'T';
+        String myString = "Papa";
+        int myNum = 12345;
+        // System.out.printf("%c", myChar);
+        System.out.println("How are you doing?");
+
+    }
 }
