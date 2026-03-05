1f4210f745058cc700b946536a1fd9c2d98dda37
nonydev <karldjansi123@gmail.com>
2026-03-05 18:52:15 +0000

    ...

--- Midsem/EmployessPay.java
+++ Midsem/EmployessPay.java
@@ -1,8 +1,37 @@
 package Midsem;
+import java.util.*;
 
 public class EmployessPay {
     public static void main(String[] args){
 
+        double grosspay = 0.00;
+        double netpay = 0.00;
+        double average_netpay = 0.00;
+        double total_netpay = 0.00;
+
+        String name = "";
+        int hours = 0;
+        int stream;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+
+        System.out.print("How many hours have you worked for? ");
+        hours = input.nextInt();
+
+        System.out.println("Please choose your stream: ");
+        System.out.println("1. JUNIOR STAFF");
+        System.out.println("2. SENIOR STAFF");
+
+        stream = input.nextInt();
+        
+        if(hours>40 && stream==1){
+        System.out.println("Please choose your stream: ");
+            
+        }
         
     }
 }
---
+++ Midsem/StudentGrade.java
@@ -0,0 +1,8 @@
+package Midsem;
+
+import java.util.*;
+public class StudentGrade {
+    public static void main(String[] args){
+        
+    }
+}
