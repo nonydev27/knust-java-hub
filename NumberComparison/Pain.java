0b8e4d6fe894c3de41e1cfaa4041efa27674cfbe
nonydev <karldjansi123@gmail.com>
2026-03-09 23:31:21 +0000

    ...

--- NumberComparison/Pain.java
+++ NumberComparison/Pain.java
@@ -1,5 +1,17 @@
 package NumberComparison;
 
 public class Pain {
-   /*Write a program with a loop that lets the user enter a series of integers. The user should enter -99 to signal the end of the series. After all the numbers have been entered, the program should display the largest and smallest numbers entered. */ 
+   /*Write a program with a loop that lets the user enter a 
+   series of integers. The user should enter -99 to signal 
+   the end of the series. After all the numbers have been entered, 
+   the program should display the largest and smallest numbers entered. */ 
+
+   public static void main(String[] args){
+    System.out.println("This program will display the largest and smallest number entered");
+    System.out.println("=================== ================= ====================");
+
+    System.out.println();
+
+    
+   }
 }
--- TemperatureTable/Pain.java
+++ TemperatureTable/Pain.java
@@ -1,8 +1,9 @@
 package TemperatureTable;
 
 public class Pain {
-    /*Write a program that displays a table of the Celsius temperatures 0 through 20 and their Fahrenheit equivalents. The formula for converting a temperature from Celsius to Fahrenheit is
+    /*Write a program that displays a table of the Celsius temperatures 0 through 20 
+    and their Fahrenheit equivalents. The formula for converting a temperature from Celsius to Fahrenheit is
 F=59​C+32
-
-where F is the Fahrenheit temperature and C is the Celsius temperature. Your program must use a loop to display the table. */
+where F is the Fahrenheit temperature and C is the Celsius temperature. Your program must use a loop to display
+ the table. */
 }
