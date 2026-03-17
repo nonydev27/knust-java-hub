e71dca63b00267926fc8c1d5ddfb31917a158dba
nonydev <karldjansi123@gmail.com>
2026-03-17 21:48:40 +0000

    ...

--- DiceGame/Main.java
+++ DiceGame/Main.java
@@ -1,5 +1,7 @@
 package DiceGame;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
         /*
@@ -19,5 +21,30 @@ This program simulates a 10-round match between a computer and a user.
         If it's a tie, no one gets a point.
 
     The Goal: After 10 rounds, compare the total wins and announce the "Grand Winner." */
-    }
+
+    String user1;
+    String user2 = "Computer";
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Welcome to the DICE GAME");
+    System.out.println("-------------------------");
+
+
+    System.out.println("Enter your name: ");
+    user1 = input.nextLine();
+    
+    System.out.println("Welcome " + user1 + " to the DICE GAME");
+
+    System.out.println("You will be playing against the computer");
+    System.out.println("-----------------------------------------");
+    System.out.println("The game will run for 10 rounds");
+    System.out.println("-----------------------------------------");
+    System.out.println("The player with the highest score wins");
+    System.out.println("-----------------------------------------");
+    System.out.println("Let's begin");
+    System.out.println("-----------------------------------------");
+
+
+    
 }
